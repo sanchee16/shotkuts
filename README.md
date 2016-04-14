7336da0f754205e3e777eec09539201054bb27e4
Sancheeta Kaushal <sanchee.k15@gmail.com>
2016-04-14 22:30:27 +0530

    Add solution for count the trailing zeroes in a factorial of a given number

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shotkuts
+
+## Description 
+
+A repo where I try to figure out seemingly different solution probably 
+using python libs. 
---
+++ count-trailing-zeroes-in-factorial.py
@@ -0,0 +1,12 @@
+## Find the number of trailing zeroes in the factorial of a number
+
+from math import factorial
+
+def find_trailing_zeroes(num):
+    num = str(factorial(num))
+    return len(num)-len(num.rstrip('0'))
+
+if __name__=="__main__":
+    number = int(raw_input("Enter the number whose count of trailing zeroes"
+    " in the factorial is to be found? \n"))
+    print find_trailing_zeroes(number)
