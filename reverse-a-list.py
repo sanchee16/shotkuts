3ca1b21ebb22e51037db6e80843620e06acb0d69
Sancheeta Kaushal <sanchee.k15@gmail.com>
2016-04-18 12:27:42 +0530

    Add code to reverse a list
    Improve the display messages

---
+++ reverse-a-list.py
@@ -0,0 +1,15 @@
+## Reverse a list 
+
+def reverse_list(input_list):
+    return input_list[::-1]
+
+if __name__=="__main__":
+    input_list = list()
+    val = True
+    while val:
+        val = raw_input("Enter list to be reversed"
+        ". Enter nothing to exit.")
+        if val:
+            input_list.append(int(val)) if val.isdigit() else input_list.append(val)
+    print ''.join(["Input List: ", str(input_list)])
+    print ''.join(["Output List: ", str(reverse_list(input_list))])
